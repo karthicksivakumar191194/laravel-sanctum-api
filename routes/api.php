9c1c33c952d8aaf3d2b0f399782c38f4145dfdf2
Karthick Sivakumar <karthicksivakumar@gmail.com>
2021-12-23 19:09:22 +0530

    [API] Login, Register, Mydetails, Logout - Routes

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\MyProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Public Routes
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/register', [RegisterController::class, 'register']);
+
+//Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::post('/logout', [LoginController::class, 'logout']);
+    Route::get('/myprofile', [MyProfileController::class, 'myProfile']);
+});
